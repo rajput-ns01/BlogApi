427ed5be062197558eeeaf4b5042efcc366e6688
rajput-ns01 <nirbhaysingh9029909678@gmail.com>
2024-11-27 12:25:35 +0530

    com

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const User = require('../model/auth')
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
